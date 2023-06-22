0a9366c1cb449af1b70381e43711a5d04431d509
Travis Gomez <frank.travis.gomez@gmail.com>
2023-06-22 12:39:44 -0500

    live clock works and re organized my file structure

---
+++ ftg/src/App.js
@@ -0,0 +1,26 @@
+import ReactDom from "react-dom/client";
+
+import React, { useState, useEffect } from 'react';
+function App() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    const name = 'Trav';
+
+    return (
+        <div>
+            My name is:
+            <h1>{name}</h1>
+            <h1>{time}</h1>
+        </div>
+    );
+}
+
+export default App;
--- ftg/src/index.js
+++ ftg/src/index.js
@@ -1,25 +1,7 @@
-//1 import react and reactDOM libraries
-import React from 'react';
 import ReactDom from 'react-dom/client';
+import App from './App';
 
-//2 get a reference to the dive with ID root
 const el = document.getElementById('root');
-
-//3 tell React to take control of that element
 const root = ReactDom.createRoot(el);
 
-//4 Create a component
-function App(){
-    const name = 'Trav';
-
-    return(
-        <div>
-            My name is:
-            <h1>{name}</h1>
-            <h1>{new Date().toLocaleTimeString()}</h1>
-        </div>
-    );
-}
-
-//5 shpw the component on the screen
 root.render(<App />);
