f0d79c3642dd2f6f0186a15ee6e764308a4a1c50
Travis Gomez <frank.travis.gomez@gmail.com>
2023-06-27 13:36:45 -0500

    added more html elements

--- ftg/src/App.js
+++ ftg/src/App.js
@@ -1,8 +1,10 @@
+import 'bulma/css/bulma.css'
 import React, { useState, useEffect } from 'react';
 import ProfileCard from './profileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
-import SiriImages from './images/siri.png';
+import SiriImage from './images/siri.png';
+
 
 function App() {
     const [time, setTime] = useState(new Date().toLocaleTimeString());
@@ -19,14 +21,26 @@ function App() {
 
     return (
         <div>
-            My name is:
+            <h1>My name is:</h1>
             <h1>{name}</h1>
             <h1>{time}</h1>
             <div>
-                Personal digital assistant
-                <ProfileCard title="Alexa" handle="@alexa" image={AlexaImage}/>
-                <ProfileCard title="Cortana" handle="@cortana" image={CortanaImage}/>
-                <ProfileCard title="Siri" handle="@siri" image={SiriImages}/>
+                <div>Personal digital assistant</div>
+                <div className="container">
+                    <section className="section">
+                        <div className="columns">
+                            <div className="column is-3">
+                                <ProfileCard title="Alexa" handle="@alexa" image={AlexaImage} />
+                            </div>
+                            <div className="column is-3">
+                                <ProfileCard title="Cortana" handle="@cortana" image={CortanaImage} />
+                            </div>
+                            <div className="column is-3">
+                                <ProfileCard title="Siri" handle="@siri" image={SiriImage} />
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </div>
         </div>
     );
